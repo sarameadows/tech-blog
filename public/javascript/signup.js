113465e2234d9a697cd6ad90e7071dfdabacc5b8
Sara <sameads9@gmail.com>
2022-09-19 20:31:42 -0500

    final touchups

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -16,7 +16,7 @@ async function signupFormHandler(event) {
         
         //check the response status
         if(response.ok) {
-            console.log(response);
+            document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
         }
